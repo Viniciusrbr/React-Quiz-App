a93f75ad0fb6529c7d4d468e885ce463aa9f2928
Vinicius Ribeiro <viniciusgoes159@gmail.com>
2023-11-22 20:37:39 -0300

    rederizando as questoes na tela

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,10 +4,12 @@ import Main from "./defaultLayout";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
   status: "loading",
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -23,13 +25,21 @@ function reducer(state, action) {
         ...state,
         status: "error",
       };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+      };
     default:
       throw new Error("Action unknown");
   }
 }
 
 function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numQuestions = questions.length;
 
@@ -47,7 +57,11 @@ function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+
+        {status == "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,9 +1,14 @@
-function StartScreen({ numQuestions }) {
+function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
       <h2>Bem Vindo ao React Quiz!</h2>
       <h3>{numQuestions} Questões para testar seu conhecimento em react</h3>
-      <button className="btn btn-ui">Vamos começar</button>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "start" })}
+      >
+        Vamos começar
+      </button>
     </div>
   );
 }
