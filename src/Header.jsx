1457bc57abbd6811e60297fdefa3b5f3a93d5c5c
Vinicius Ribeiro <viniciusgoes159@gmail.com>
2023-11-22 19:48:42 -0300

    lendo dados da api fake

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="app-header">
+      <img src="ReactLogo.png" alt="React logo" />
+      <h1>The React FQuiz</h1>
+    </header>
+  );
+}
+
+export default Header;
