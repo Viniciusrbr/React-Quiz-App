e9fc5d1382b38582bf278b72ad58a5e056887591
Vinicius Ribeiro <viniciusgoes159@gmail.com>
2023-11-23 16:20:37 -0300

    funcionalidade de finalizar e reiniciar o quizz adicionada

---
+++ src/components/FinishScreen.jsx
@@ -0,0 +1,30 @@
+function FinishScreen({ points, maxPossiblePoints, highscore, dispatch }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 80 && percentage < 100) emoji = "🎉";
+  if (percentage >= 50 && percentage < 80) emoji = "🙃";
+  if (percentage >= 0 && percentage < 50) emoji = "🤨";
+  if (percentage === 0) emoji = "🤦‍♂️";
+
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span> Sua pontuação <strong>{points}</strong> de{" "}
+        {maxPossiblePoints} ({Math.ceil(percentage)}%)
+      </p>
+
+      <p className="highscore">Pontuação máxima: {highscore} pontos</p>
+
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Reiniciar Quizz
+      </button>
+    </>
+  );
+}
+
+export default FinishScreen;
